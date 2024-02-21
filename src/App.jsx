cc5872b882fc76f70b6a0d8a25e33d3788a9eded
Viacheslav Kostenko <kostenko.uvd@gmail.com>
2024-02-21 17:29:53 +0200

    fetched users data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { useState, useEffect } from "react";
 import UserCard from "./components/UserCard/UserCard";
+import axios from "axios";
+
+const BASE_URL = "https://65d4fa523f1ab8c634366212.mockapi.io/users";
 
 function App() {
+  const [userData, setUserData] = useState(null);
+  console.log(userData);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(BASE_URL);
+        setUserData(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="container">
-      <UserCard />
+      {userData && <UserCard userData={userData} />}
     </div>
   );
 }
--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -3,21 +3,30 @@ import logo from "../../assets/logo-goit.png";
 import mainImg from "../../assets/main-img.png";
 import defaultAvatar from "../../assets/default-avatar.jpg";
 
-const UserCard = () => {
+const UserCard = ({ userData }) => {
   return (
-    <div className={s.userCard}>
-      <img className={s.logo} src={logo} alt="logo" />
-      <img className={s.mainImg} src={mainImg} alt="main image" />
-      <div className={s.avatarBone}></div>
-      <div className={s.avatarImgWrapper}>
-        <img className={s.avatarImg} src={defaultAvatar} alt="" />
-      </div>
-      <div className={s.informWrapper}>
-        <p className={s.informTweets}>777 tweets</p>
-        <p className={s.informFollowers}>100,501 Followers</p>
-        <button className={s.followBtn}>Follow</button>
-      </div>
-    </div>
+    <>
+      {userData.map((user) => (
+        <div className={s.userCard} key={user.id}>
+          <img className={s.logo} src={logo} alt="logo" />
+          <img className={s.mainImg} src={mainImg} alt="image" />
+          <div className={s.avatarBone}></div>
+          <div className={s.avatarImgWrapper}>
+            <img
+              className={s.avatarImg}
+              src={user.avatar ? user.avatar : defaultAvatar}
+              alt="user avatar"
+            />
+          </div>
+          <div className={s.informWrapper}>
+            <p className={s.userName}>{user.user}</p>
+            <p className={s.informTweets}>{user.tweets} tweets</p>
+            <p className={s.informFollowers}>{user.followers} Followers</p>
+            <button className={s.followBtn}>Follow</button>
+          </div>
+        </div>
+      ))}
+    </>
   );
 };
 
