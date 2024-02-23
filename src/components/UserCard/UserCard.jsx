05b86c037398376eb92cd5cb12fff725bf8fc8b8
Viacheslav Kostenko <kostenko.uvd@gmail.com>
2024-02-23 13:11:57 +0200

    fixed style in UserCatd

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -47,37 +47,39 @@ const UserCard = ({ displayedUsers, followers, setFollowers, BASE_URL }) => {
     return count >= 10000 ? count.toLocaleString("en-US") : count;
   };
   return (
-    <div className={s.userCardWrapper}>
+    <>
       <ToastContainer />
-      {displayedUsers.map((user) => (
-        <div className={s.userCard} key={nanoid()}>
-          <img className={s.logo} src={logo} alt="logo" />
-          <img className={s.mainImg} src={mainImg} alt="image" />
-          <div className={s.avatarBone}></div>
-          <div className={s.avatarImgWrapper}>
-            <img
-              className={s.avatarImg}
-              src={user.avatar ? user.avatar : defaultAvatar}
-              alt="user avatar"
-            />
+      <div className={s.userCardWrapper}>
+        {displayedUsers.map((user) => (
+          <div className={s.userCard} key={nanoid()}>
+            <img className={s.logo} src={logo} alt="logo" />
+            <img className={s.mainImg} src={mainImg} alt="image" />
+            <div className={s.avatarBone}></div>
+            <div className={s.avatarImgWrapper}>
+              <img
+                className={s.avatarImg}
+                src={user.avatar ? user.avatar : defaultAvatar}
+                alt="user avatar"
+              />
+            </div>
+            <div className={s.informWrapper}>
+              <p className={s.userName}>{user.user}</p>
+              <p className={s.informTweets}>{user.tweets} tweets</p>
+              <p className={s.informFollowers}>
+                {followers[user.id] && formatFollowers(followers[user.id])}{" "}
+                Followers
+              </p>
+              <button
+                className={isActive[user.id] ? s.followBtnActive : s.followBtn}
+                onClick={() => handleClickActive(user.id)}
+              >
+                {isActive[user.id] ? "Following" : "Follow"}
+              </button>
+            </div>
           </div>
-          <div className={s.informWrapper}>
-            <p className={s.userName}>{user.user}</p>
-            <p className={s.informTweets}>{user.tweets} tweets</p>
-            <p className={s.informFollowers}>
-              {followers[user.id] && formatFollowers(followers[user.id])}{" "}
-              Followers
-            </p>
-            <button
-              className={isActive[user.id] ? s.followBtnActive : s.followBtn}
-              onClick={() => handleClickActive(user.id)}
-            >
-              {isActive[user.id] ? "Following" : "Follow"}
-            </button>
-          </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   );
 };
 
